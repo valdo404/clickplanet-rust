32b4c50b614803f31276d0423b3ce02892e25b7d
Laurent Valdes <laurent@lapoule.dev>
2024-12-21 19:45:59 +0100

    feat: add Modal dialog

--- clickplanet-webapp/src/main.rs
+++ clickplanet-webapp/src/main.rs
@@ -18,6 +18,9 @@ fn App() -> Element {
                 log::info!("Button clicked!");
             }
         }
+        DiscordButton {
+            message: Some("Join our Discord server".to_string()),
+        }
     }
 }
 
@@ -103,6 +106,50 @@ pub fn DiscordButton(props: DiscordButtonProps) -> Element {
             class: "button button-discord",
             aria_label: "{message}",
             "{message}"
+        }
+    )
+}
+
+#[derive(Props, Clone, PartialEq)]
+pub struct ModalProps {
+    #[props(optional)]
+    pub title: Option<String>,             // Optional title for the modal
+    pub children: Element,                // Modal content
+    pub on_close: Callback<()>,           // Callback for when the modal is closed
+    #[props(optional)]
+    pub close_button_text: Option<String>, // Optional text for the close button
+}
+
+#[component]
+pub fn Modal(props: ModalProps) -> Element {
+    rsx!(
+        div {
+            class: "modal",
+            onclick: move |_| props.on_close.call(()), // Close the modal when clicking outside
+
+            div {
+                class: "modal-content",
+                onclick: move |evt| evt.stop_propagation(),
+
+                match &props.title {
+                    Some(title) => rsx!(
+                        div {
+                            class: "modal-header",
+                            h2 { "{title}" }
+                        }
+                    ),
+                    None => rsx!()
+                },
+
+                {
+                    props.children
+                },
+
+                CloseButton {
+                    text: props.close_button_text.clone(),
+                    on_click: move |_| props.on_close.call(()),
+                }
+            }
         }
     )
 }
